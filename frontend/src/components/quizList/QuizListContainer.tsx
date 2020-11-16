b12c10e333de098b03c0bd68e01db933c3c75204
r33an1 <r33an1@me.com>
2020-11-16 18:02:01 +0100

    Further cleanup

--- frontend/src/components/quizList/QuizListContainer.tsx
+++ frontend/src/components/quizList/QuizListContainer.tsx
@@ -20,9 +20,9 @@ const QuizList = () => {
     const columns = [
         { field: "title", headerName: "Name", width: 150 },
         { field: "genre", headerName: "Genre", width: 150 },
-        { field: "songsLength", headerName: "Songs", width: 130 },
-        { field: "creator", headerName: "Created By", width: 150 },
-        { field: "createdAt", headerName: "Created At", width: 150 },
+        { field: "songsLength", headerName: "Songs", width: 80 },
+        { field: "creator", headerName: "Created By", width: 110 },
+        { field: "createdAt", headerName: "Created At", width: 240 },
     ];
 
     const rowClick = (event) => {
